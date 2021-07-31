cd94e8dbee71b6d1dae70169c52e6432866ac630
baraneetharan <baraneetharan@live.com>
2021-08-01 00:45:25 +0530

    html2approvalsflow.herokuapp.com url used

--- wwwroot/test.js
+++ wwwroot/test.js
@@ -1,4 +1,5 @@
-const getPendingApprovaluri = "https://localhost:5001/api/Approval";
+// const getPendingApprovaluri = "https://localhost:5001/api/Approval";
+const getPendingApprovaluri = "https://html2approvalsflow.herokuapp.com/api/Approval";
 const triggerApprovalsuri =
   "https://prod-20.southeastasia.logic.azure.com:443/workflows/6627b04f257c4c7ca6e612f703244360/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pStRj1LvVV9bSVWqhRF9-nDle9lGGQlpj4hfwXkmuws";
 
